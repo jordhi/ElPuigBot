572de5da845fbf6cc84681e3ef06b6cd523ec558
jordhi <jordhi@gmail.com>
2016-12-13 12:04:49 +0100

    updates gitignore

--- src/config/BotConfig.java
+++ src/config/BotConfig.java
@@ -5,8 +5,8 @@ package config;
  * TOKENs and USERs config
  */
 public class BotConfig {
-    public static final String TOKEN_COMMAND = "TOKEN";
-    public static final String USER_COMMAND = "USER";
+    public static final String TOKEN_COMMAND = "TOKEN_BOT";
+    public static final String USER_COMMAND = "USER_BOT";
 
 
 
