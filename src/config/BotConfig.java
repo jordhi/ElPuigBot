60a186e2b7b566478fccfd172fabac26dc11b41b
jordhi <jordhi@gmail.com>
2016-12-13 11:42:29 +0100

    preparar BotConfig

--- src/config/BotConfig.java
+++ src/config/BotConfig.java
@@ -5,11 +5,9 @@ package config;
  * TOKENs and USERs config
  */
 public class BotConfig {
-    public static final String TOKEN_COMMAND = "326380516:AAHY6K2tIe6gt-E747Isur1UF9nm_P3i9tA";
-    public static final String USER_COMMAND = "ElPuigBetaBot";
+    public static final String TOKEN_COMMAND = "TOKEN";
+    public static final String USER_COMMAND = "USER";
 
-   // public static final String TOKEN_ELPUIG = "318046886:AAGXDTEGesXuxhgV1SE-qwxYjB5VSKa_v88";
-   // public static final String USER_ELPUIG = "ep_userbot";
 
 
 }
