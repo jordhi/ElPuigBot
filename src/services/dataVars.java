3c85417dc0e5189727ffbc0179244d23672ad52b
jordhi <jordhi@gmail.com>
2018-09-14 22:06:25 +0200

    actualitzar nom pdfs horaris alumnes

--- src/services/dataVars.java
+++ src/services/dataVars.java
@@ -8,17 +8,17 @@ public class dataVars {
     public static final String ROOTDOCS = "//home/telegrambot/ElPuigBot/docs/";
 
     //Horaris Grups
-    public static final String HSMX1A = ROOTDOCS + "HorariSMX1A17-18.pdf";
-    public static final String HSMX1B = ROOTDOCS + "HorariSMX1B17-18.pdf";
-    public static final String HSMX1C = ROOTDOCS + "HorariSMX1C17-18.pdf";
-    public static final String HSMX2A = ROOTDOCS + "HorariSMX2A17-18.pdf";
-    public static final String HSMX2B = ROOTDOCS + "HorariSMX2B17-18.pdf";
-    public static final String HSMX2C = ROOTDOCS + "HorariSMX2C17-18.pdf";
-    public static final String HGS1A = ROOTDOCS + "HorariDAM1A17-18.pdf";
-    public static final String HGS1B = ROOTDOCS + "HorariDAM1B17-18.pdf";
-    public static final String HDAM2A = ROOTDOCS + "HorariDAM2A17-18.pdf";
-    public static final String HDAM2B = ROOTDOCS + "HorariDAM2B17-18.pdf";
-    public static final String HASIX2A = ROOTDOCS + "HorariASIX217-18.pdf";
+    public static final String HSMX1A = ROOTDOCS + "Horari1SMXA1819.pdf";
+    public static final String HSMX1B = ROOTDOCS + "Horari1SMXB1819.pdf";
+    public static final String HSMX1C = ROOTDOCS + "Horari1SMXC1819.pdf";
+    public static final String HSMX2A = ROOTDOCS + "Horari2SMXA1819.pdf";
+    public static final String HSMX2B = ROOTDOCS + "Horari2SMXB1819.pdf";
+    public static final String HSMX2C = ROOTDOCS + "Horari2SMXC1819.pdf";
+    public static final String HGS1A = ROOTDOCS + "Horari1ASIX1819.pdf";
+    public static final String HGS1B = ROOTDOCS + "Horari1DAM1819.pdf";
+    public static final String HDAM2A = ROOTDOCS + "Horari2DAMA1819.pdf";
+    public static final String HDAM2B = ROOTDOCS + "Horari2DAMB1819.pdf";
+    public static final String HASIX2A = ROOTDOCS + "Horari2ASIX1819.pdf";
 
     //Horaris Professors
     public static final String HPDilluns = ROOTDOCS + "dilluns.pdf";
